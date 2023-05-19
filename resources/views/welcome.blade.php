eeb2508ead8faed5e3ee9351c3d514792982a34e
yasinrahmatian <yasinrahmatian95@gmail.com>
2023-05-19 14:15:39 +0330

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsors
+                                Sponsorss
                             </a>
                         </div>
                     </div>
